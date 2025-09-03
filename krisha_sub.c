1e3774c25526b1d487e2a656f1fbaf8ada6f1110
krisha0509 <krisharathod772@gmail.com>
2025-09-03 12:42:46 +0530

    Add files via upload

---
+++ krisha_sub.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int num1,num2,sub;
+
+printf("Enter the first number:");
+scanf("%d",&num1);
+
+printf("Enter the second number:");
+scanf("%d",&num2);
+
+sub=num1-num2;
+
+printf("this is sub: %d\n",sub);
+return 0;
+
+}
