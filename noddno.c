9e97f909de56f02915dd3ed27896c662c2aabd78
Krisha Rathod <krisharathod772@gmail.com>
2025-10-02 11:57:21 +0530

    Add files via upload

---
+++ noddno.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+int  i,n;
+printf("Enter the value of n:");
+scanf("%d",&n);
+printf("First %d odd numbers:\n",n);
+for(i=1;i<=2*n;i+=2){
+printf("%d",i);}
+return 0;
+}
