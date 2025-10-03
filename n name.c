cb8e95f5afccc064788837d500e3beca04a5cef9
Krisha Rathod <krisharathod772@gmail.com>
2025-10-03 18:16:24 +0530

    Add files via upload

---
+++ n name.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+int n,i;
+printf("Enter how many times you want to print your name:");
+scanf("%d",&n);
+for(i=1;i<=n;i++){
+printf("Krisha Rathod\n");
+}
+return 0;
+}
